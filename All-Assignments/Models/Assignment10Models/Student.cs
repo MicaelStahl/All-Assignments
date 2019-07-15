d2e7390a75cc26e66b280d4a44c9379a429e21af
micael Ståhl <micael_stahl96@hotmail.com>
2019-07-15 10:56:17 +0200

    slight progress.

---
+++ All-Assignments/Models/Assignment10Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace All_Assignments.Models.Assignment10Models
+{
+    public class Student
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "The firstname has to be between 2 to 20 characters long.")]
+        [Display(Name = "Firstname")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "The lastname has to be between 2 to 30 characters long.")]
+        [Display(Name = "Lastname")]
+        public string LastName { get; set; }
+
+
+    }
+}
